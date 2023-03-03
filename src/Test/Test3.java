cd6c74dc5c7ddfdfbbe409562ee823cea0f870c4
AleksPet <p.aleksandra83@gmail.com>
2023-03-03 14:48:16 -0500

    ///

--- src/Test/Test3.java
+++ src/Test/Test3.java
@@ -2,8 +2,7 @@ package Test;
 
 public class Test3 {
 
-
-   //hot fix of the failed tests
-
-    // hor fix 2
+    public static void main(String[] args) {
+        System.out.println("pull/push");
+    }
 }
